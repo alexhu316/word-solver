06a184f7fb31569e4b0345e85566f7ca2b9f3de3
alexhu316 <77501388+alexhu316@users.noreply.github.com>
2021-01-16 15:20:06 -0500

    Update anagramSolver.java

--- src/anagramSolver.java
+++ src/anagramSolver.java
@@ -3,127 +3,10 @@ import java.io.*;
 
 public class anagramSolver {
 
-
-	public static int CHARBREAK = 70;
 	public static int MAXLETTERS = 12;
 	public static int MINLETTERS = 4;
-
-	//	//Recursive algorithm which chooses one letter from the set of letters 
-	//	//at a time, eventually resulting in every 'n' letter combination
-	//	//used when the letters wanted is closer to 0
-	//	public static void addLetter (String starting, String lettersLeft, int n, ArrayList<String> list) {
-	//		if (starting.length()==n) {
-	//			boolean go = true;
-	//			for (int i = 0; i<list.size(); i++) {
-	//				if (isAnagram(list.get(i),starting)) {
-	//					go = false;
-	//					break;
-	//				}
-	//			}
-	//			if(go) {
-	//				list.add(starting);
-	//			}
-	//		}
-	//		else {
-	//			for (int i = 0; i<lettersLeft.length(); i++) {
-	//				addLetter(starting+lettersLeft.charAt(i), lettersLeft.substring(0,i)+lettersLeft.substring(Math.min(lettersLeft.length(), i+1),lettersLeft.length()), n, list);
-	//			}
-	//		}
-	//	}
-	//
-	//	//Recursive algorithm which removes one letter from the set of letters 
-	//	//at a time, eventually resulting in every 'n' letter combination
-	//	//used when the letters wanted is closer to the total number of letters
-	//	public static void removeLetter (String lettersLeft, int n, ArrayList<String> list) {
-	//
-	//		if (lettersLeft.length()==n) {
-	//			boolean go = true;
-	//			for (int i = 0; i<list.size(); i++) {
-	//				if (isAnagram(list.get(i),lettersLeft)) {
-	//					go = false;
-	//					break;
-	//				}
-	//			}
-	//			if(go) {
-	//				list.add(lettersLeft);
-	//			}
-	//		}
-	//
-	//		else {
-	//			for (int i = 0; i<lettersLeft.length(); i++) {
-	//				removeLetter(lettersLeft.substring(0,i)+lettersLeft.substring(Math.min(lettersLeft.length(), i+1),lettersLeft.length()), n, list);
-	//
-	//			}
-	//		}
-	//	}
-	//
-	//
-	//	//determines whether two series of letters are anagrams
-	//	//by recording the number of each letter and comparing
-	//	public static boolean isAnagram (String dict, String word) {
-	//		if (dict.length()!=word.length()) {
-	//			return false;
-	//		}
-	//		int[] dictLetters = new int[26];
-	//		int[] wordLetters = new int[26];
-	//
-	//		for (int i = 0; i<dict.length(); i++) {
-	//			dictLetters[(int)dict.charAt(i)-97]++;
-	//			wordLetters[(int)word.charAt(i)-97]++;
-	//		}
-	//
-	//		for (int i = 0; i<26; i++) {
-	//			if (dictLetters[i]!=wordLetters[i]) {
-	//				return false;
-	//			}
-	//		}
-	//		return true;
-	//	}
-	//
-	//
-	//	//Finds and prints all anagrams of 'n' letters 
-	//	//that can me made from the input word.
-	//	public static void printAnas(String word, int n, List<String>words){
-	//
-	//		ArrayList<String> anagrams = new ArrayList<String>(); //contains all real words anagrams of 'n' letters 
-	//		ArrayList<String> combos = new ArrayList<String>(); //contains all sets of letters of 'word' of 'n' letters 
-	//
-	//		if ((word.length())-n>word.length()/2) {
-	//			addLetter("",word,n,combos);
-	//		}
-	//		else {
-	//			removeLetter(word,n,combos);
-	//		}
-	//
-	//		for (int i = 0; i<combos.size(); i++) {
-	//			for (int j = 0; j<words.size(); j++) {
-	//				if (isAnagram(combos.get(i),words.get(j))) {
-	//					anagrams.add(words.get(j));
-	//				}
-	//			}
-	//		}		
-	//
-	//		Collections.sort(anagrams);
-	//
-	//		int counter =0;
-	//		if (anagrams.size()>0) {
-	//			System.out.println("\n");
-	//
-	//			System.out.println(n+" letters:");
-	//			System.out.print("\t");
-	//
-	//			for (int i = 0 ; i<anagrams.size(); i++) {
-	//				System.out.print(anagrams.get(i)+"  ");
-	//				counter+=anagrams.get(i).length()+2;
-	//				if (counter>CHARBREAK&&i+1<anagrams.size()) {
-	//					counter=0;
-	//					System.out.println();
-	//					System.out.print("\t");
-	//				}
-	//			}
-	//		}
-	//	}
-
+	
+	public static int CHARBREAK = 70;
 
 	//determines whether a dictionary word is contained within a given word
 	public static boolean isAnagram (String dict, String word) {
@@ -146,7 +29,8 @@ public class anagramSolver {
 	}
 
 
-	//
+	//Finds and prints all anagrams of all lengths
+	//of a given word, formatting by length and by alphabet
 	public static void printAnas(String word, List<String>words){
 
 		ArrayList<String> anagrams = new ArrayList<String>();
@@ -160,7 +44,7 @@ public class anagramSolver {
 		}
 
 		Collections.sort(anagrams);
-		Collections.sort(anagrams, C);
+		Collections.sort(anagrams, compareLength);
 
 		int counter=0;
 		int formatLetters=MAXLETTERS+1;
@@ -182,8 +66,8 @@ public class anagramSolver {
 		}
 	}
 
-	//Allows the ArrayList to be sorted by size
-	static Comparator<String> C = new Comparator <String>(){
+	//Allows the ArrayList to be sorted by length
+	static Comparator<String> compareLength = new Comparator <String>(){
 		public int compare(String s1, String s2) {
 			return Integer.compare(s2.length(), s1.length());
 		}
@@ -215,19 +99,10 @@ public class anagramSolver {
 		MINLETTERS = in.nextInt();
 
 		long start = System.currentTimeMillis();
-
-		//finds anagrams of the word for every letter size
-		//between the maximum and minimum number of letters 
-		//		for(int i = Math.min(high, word.length()); i>=low; i--) {
-		//			printAnas(word,i,words);
-		//		}
-
-
+		
 		printAnas(word,words);
 
 		long finish = System.currentTimeMillis();
-
-
 
 		System.out.println("\n\nElapsed time: "+((finish-start)/1000.0)+"s");
 
